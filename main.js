786275e0722df9073cbfe9bdc0838b868e6b168b
mellcontadeestudos <mellcontadeestudos@gmail.com>
2025-04-02 05:42:25 +0000

    Criação da função para calcular todos os resultados

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
+
+    calculateResults()
 }
 
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
+
+    calculateResults()
 }
 
 function receiveTipPercentageValue(value) {
@@ -23,17 +27,32 @@ function receiveTipPercentageValue(value) {
     buttonSelected = document.querySelector(`#button-${value}`)
     console.log(buttonSelected)
     buttonSelected.classList.add("button-selected")
+
+    calculateResults()
 }
 
 function receiveCustonTipPercentageValue() {
     tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
 
     removeClassButtonSelected()
+    calculateResults()
 }
 
 function removeClassButtonSelected() {
     if (buttonSelected !== null) {
         buttonSelected.classList.remove("button-selected")
         buttonSelected = null
+    }
+}
+
+function calculateResults() {
+    if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+        let strongAmount = document.querySelector(".amount strong")
+        let tipAmountPerson = bill * tipPercentage / numberOfPeople
+        strongAmount.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+        let strongTotal = document.querySelector(".total strong")
+        let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+        strongTotal.innerText = `$${totalPerson.toFixed(2)}`
     }
 }
